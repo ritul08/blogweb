45d375a50f8705b54ae30993b2e1fa3a3e4d6750
ritul22 <128178487+ritul08@users.noreply.github.com>
2023-12-26 22:20:11 +0530

    Add files via upload

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../components/Home/Header';
+import SearchBar from '../../components/Home/SearchBar';
+const Home = () => {
+  return (
+    <div>
+      {/*Page Header*/}
+      <Header/>
+      {/*Search Bar*/}
+      <SearchBar/>
+    </div>
+  )
+}
+
+export default Home;
